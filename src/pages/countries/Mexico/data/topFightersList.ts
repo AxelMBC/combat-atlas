ec5cbc765678d62f2b01d410b22bd09ffba46522
El Coflas <barrazaaxel2000@gmail.com>
2026-02-22 23:27:07 -0700

    feat: :sparkles: local import of top fighters images instead of image reference

--- src/pages/countries/Mexico/data/topFightersList.ts
+++ src/pages/countries/Mexico/data/topFightersList.ts
@@ -1,7 +1,10 @@
 import type { fighterType } from "@/types/fighterType";
-import JulioCesarImg from "../resources/Fighters/topFighterJulioCesar.avif";
-import CaneloAlvarezImg from "../resources/Fighters/topFighterCaneloAlvarez.webp";
-import SalvadorSanchezImg from "../resources/Fighters/SalvadorSanchez.jpg";
+
+import julioCesarImg from "../resources/fighters/julioCesar.avif";
+import caneloAlvarezImg from "../resources/fighters/caneloAlvarez.webp";
+import ricardoLopezImg from "../resources/fighters/ricardoLopez.avif";
+import juanManuelMarquezImg from "../resources/fighters/juanMarquez.webp";
+import salvadorSanchezImg from "../resources/fighters/salvadorSanchez.jpg";
 
 export const topFightersData: fighterType[] = [
   {
@@ -9,7 +12,7 @@ export const topFightersData: fighterType[] = [
     name: "Julio César Chávez",
     record: "107-6-2",
     nickName: "El Gran Campeón Mexicano",
-    image: JulioCesarImg,
+    image: julioCesarImg,
     fightsCounter: 5,
   },
   {
@@ -17,7 +20,7 @@ export const topFightersData: fighterType[] = [
     name: "Canelo Álvarez",
     record: "61-2-2",
     nickName: "Rey Indiscutido",
-    image: CaneloAlvarezImg,
+    image: caneloAlvarezImg,
     fightsCounter: 5,
   },
   {
@@ -25,8 +28,7 @@ export const topFightersData: fighterType[] = [
     name: "Ricardo López",
     record: "51-0-1",
     nickName: "Finito, La Perfección",
-    image:
-      "https://www.infobae.com/resizer/v2/6AMVEHZNYVGHJGXDUVFYHCJNRQ.jpg?auth=673c35eeb291c91cd8d7928c4a4943f64ffe929c7200e7715ec93f8e4afd8155",
+    image: ricardoLopezImg,
     fightsCounter: 5,
   },
   {
@@ -34,8 +36,7 @@ export const topFightersData: fighterType[] = [
     name: "Juan Manuel Márquez",
     record: "56-7-1",
     nickName: "Maestro del Contragolpe",
-    image:
-      "https://i0.wp.com/wbanews.com/wp-content/uploads/2013/01/Juan-Manuel-Marquez-dejo-a-Latinoamerica-en-lo-mas-alto.jpg",
+    image: juanManuelMarquezImg,
     fightsCounter: 5,
   },
   {
@@ -43,46 +44,7 @@ export const topFightersData: fighterType[] = [
     name: "Salvador Sánchez",
     record: "44-1-1",
     nickName: "El Campeón Inmortal",
-    image: SalvadorSanchezImg,
     fightsCounter: 5,
-  },
-];
-
-export const boxersSearch = [
-  {
-    id: "1",
-    name: "Julio César Chávez",
-  },
-  {
-    id: "2",
-    name: "Canelo Álvarez",
-  },
-  {
-    id: "3",
-    name: "Ricardo López",
-  },
-  {
-    id: "4",
-    name: "Juan Manuel Márquez",
-  },
-  {
-    id: "5",
-    name: "Salvador Sánchez",
-  },
-  {
-    id: 6,
-    name: "Rubén 'Púas' Olivares",
-  },
-  {
-    id: 7,
-    name: "Érik 'El Terrible' Morales",
-  },
-  {
-    id: 8,
-    name: "Marco Antonio Barrera",
-  },
-  {
-    id: 9,
-    name: "Carlos Zárate",
+    image: salvadorSanchezImg,
   },
 ];
--- src/pages/countries/Thailand/data/topFightersList.ts
+++ src/pages/countries/Thailand/data/topFightersList.ts
@@ -1,12 +1,18 @@
 import type { fighterType } from "@/types/fighterType";
 
+import namsaknoiYudthagarngamtornImg from "../resources/fighters/namsaknoiYudthagarngamtorn.jpg";
+import suphachaiSaepongImg from "../resources/fighters/suphachaiSaepong.jpg";
+import samartPayakaroonImg from "../resources/fighters/samartPayakaroon.webp";
+import dieselnoiChorThanasukarnImg from "../resources/fighters/diezelnoiChorThanasukarn.jpeg";
+import apidejSitHirun from "../resources/fighters/apidejSit-Hirun.jpeg";
+
 export const topFightersData: fighterType[] = [
   {
     id: "1",
     name: "Namsaknoi Yudthagarngamtorn",
     record: "280-15-5",
     nickName: "El Emperador",
-    image: "https://gofind.jjtravel.com/26657",
+    image: namsaknoiYudthagarngamtornImg,
     fightsCounter: 5,
   },
   {
@@ -14,8 +20,7 @@ export const topFightersData: fighterType[] = [
     name: "Suphachai Saepong",
     record: "313-41-5",
     nickName: "Saenchai",
-    image:
-      "https://upload.wikimedia.org/wikipedia/commons/thumb/1/1f/SaenchaiSorKingstar.jpg/1280px-SaenchaiSorKingstar.jpg",
+    image: suphachaiSaepongImg,
     fightsCounter: 5,
   },
   {
@@ -23,8 +28,7 @@ export const topFightersData: fighterType[] = [
     name: "Samart Payakaroon",
     record: "130-18-2",
     nickName: "El Muhammed Ali de Muay Thai",
-    image:
-      "https://cdn.evolve-vacation.com/wp-content/uploads/2019/06/Samart-Payakaroon.jpg",
+    image: samartPayakaroonImg,
     fightsCounter: 5,
   },
   {
@@ -32,8 +36,7 @@ export const topFightersData: fighterType[] = [
     name: "Dieselnoi Chor Thanasukarn",
     record: "110-5-2",
     nickName: "El rey the las rodillas",
-    image:
-      "https://images.tapology.com/letterbox_images/370862/default/BE7C2DAA-16AF-4F8A-A2A3-2816B43B4012.jpeg?1674407913",
+    image: dieselnoiChorThanasukarnImg,
     fightsCounter: 5,
   },
   {
@@ -41,47 +44,7 @@ export const topFightersData: fighterType[] = [
     name: "Apidej Sit-Hirun",
     record: "340-10-1",
     nickName: "La patada de la muerte",
-    image:
-      "https://cdn.evolve-mma.com/wp-content/uploads/2019/09/Apidej-Sit-Hirun-Muay-THai.png",
+    image: apidejSitHirun,
     fightsCounter: 5,
-  },
-];
-
-export const boxersSearch = [
-  {
-    id: "1",
-    name: "Julio César Chávez",
-  },
-  {
-    id: "2",
-    name: "Canelo Álvarez",
-  },
-  {
-    id: "3",
-    name: "Ricardo López",
-  },
-  {
-    id: "4",
-    name: "Juan Manuel Márquez",
-  },
-  {
-    id: "5",
-    name: "Salvador Sánchez",
-  },
-  {
-    id: 6,
-    name: "Rubén 'Púas' Olivares",
-  },
-  {
-    id: 7,
-    name: "Érik 'El Terrible' Morales",
-  },
-  {
-    id: 8,
-    name: "Marco Antonio Barrera",
-  },
-  {
-    id: 9,
-    name: "Carlos Zárate",
   },
 ];
