62c6f36c3cace579950d52bd070c925996754df7
El Coflas <barrazaaxel2000@gmail.com>
2026-04-29 01:13:33 -0600

    feat: :sparkles: update fighter images

--- src/pages/Thailand/resources/fighters/index.ts
+++ src/pages/Thailand/resources/fighters/index.ts
@@ -1,15 +1,15 @@
 import namsaknoiYudthagarngamtornImg from "./namsaknoi_yudthagarngamtorn.jpg";
-import suphachaiSaepongImg from "./suphachai_saepong.jpg";
+import saenchai from "./saenchai.jpg";
 import samartPayakaroonImg from "./samart_payakaroon.webp";
 import dieselnoiChorThanasukarnImg from "./dieselnoi_chor_thanasukarn.jpeg";
-import apidejSitHirunImg from "./apidej_sit-hirun.webp";
+import apidejSitHirunImg from "./apidej_sit_hirun.jpg";
 
 const fighterImages: Record<string, string> = {
   namsaknoi_yudthagarngamtorn: namsaknoiYudthagarngamtornImg,
-  suphachai_saepong: suphachaiSaepongImg,
+  saenchai: saenchai,
   samart_payakaroon: samartPayakaroonImg,
   dieselnoi_chor_thanasukarn: dieselnoiChorThanasukarnImg,
-  "apidej_sit-hirun": apidejSitHirunImg,
+  apidej_sit_hirun: apidejSitHirunImg,
 };
 
 export const getFighterImage = (filename: string): string =>
